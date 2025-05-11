fd2535a40f5a3697a40f9b5655a2e9cf657c54fc
AlgoClassWork <kyxec.play@gmail.com>
2025-05-11 13:51:04 +0600

    Update urls.py

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from product.views import product_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', product_list, name='product_list'),
+    path('<slug:category_slug>', product_list, name='product_list_by_category')
 ]
