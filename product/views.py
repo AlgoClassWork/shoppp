35b4b59abcf91671c8bb6ebb1077980f5d8b50db
AlgoClassWork <kyxec.play@gmail.com>
2025-05-11 13:51:20 +0600

    Update views.py

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products_list = Product.objects.filter(availabel=True)
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        product_list = Product.objects.filter(category=category)
+
+    return render(request, 'list.html', {'products' : product_list,
+                                         'category': category,
+                                         'categories': categories})
